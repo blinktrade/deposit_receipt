7d60955871e82a6f99bf8ed5717581cc2b855c16
Rodrigo Souza <rodrigo@Rodrigos-Mac-mini.local>
2017-05-29 09:10:06 -0400

    fix url domain

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var config = {
   blinktrade: {
-    prod: 'https://depositsapi.blinktrade.com/_webhook/deposit_receipt',
+    prod: 'https://api.blinktrade.com/_webhook/deposit_receipt',
     testnet: 'https://api.testnet.blinktrade.com/_webhook/deposit_receipt',
   },
   store_url: 'https://storage.googleapis.com/%s/%s'
