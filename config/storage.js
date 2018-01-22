5d0933f1889b2cef940b56fdace87a437c9f73aa
Cesar Augusto <cesardeazevedo@outlook.com>
2018-01-22 18:51:15 -0200

    Remove projectId and GCLOUD_PROJECT env variable

--- config/storage.js
+++ config/storage.js
@@ -3,9 +3,7 @@ var Storage = require('@google-cloud/storage');
 // The following environment variables are set by app.yaml when running on GAE,
 // but will need to be manually set when running locally.
 // The storage client is used to communicate with Google Cloud Storage
-var storage = new Storage({
-  projectId: process.env.GCLOUD_PROJECT
-});
+var storage = Storage();
 
 // A bucket is a container for objects (files).
 var bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
