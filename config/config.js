8c77bdff5f6433f93cddd1cc371c844c147aaf9e
Rodrigo Souza <pinhopro@users.noreply.github.com>
2019-02-18 07:25:52 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ var config = {
   blinktrade: {
     prod: 'https://api.blinktrade.com/_webhook/deposit_receipt',
     bitcambio: 'https://bitcambio_api.blinktrade.com/_webhook/deposit_receipt',
-    testnet: 'https://api.testnet.blinktrade.com/_webhook/deposit_receipt',
+    testnet: 'https://api_testnet.blinktrade.com/_webhook/deposit_receipt',
   },
   store_url: 'https://storage.googleapis.com/%s/%s'
 };
